45408c0f0b05b8d51c4e3d1938dcd0ac0ab5f3ef
PeNDy <b10923024@gemail.yuntech.edu.tw>
2022-05-29 06:03:17 +0800

    0529 v11 Add ECG

---
+++ app/src/main/java/com/example/twapp/ElectroCardioGraphy.java
@@ -0,0 +1,14 @@
+package com.example.twapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ElectroCardioGraphy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_electro_cardio_graphy);
+    }
+}
