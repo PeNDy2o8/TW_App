e81ed9f153841894d56cf9f8506b4ffbd6a22456
zx3282 <zx32829601@gmail.com>
2022-12-15 22:43:58 +0800

    //todo、內部跟資料庫的溝通 //Done 登入頁面、側拉選單、創帳號的頁面

---
+++ app/src/main/java/com/example/twapp/CreateAcount.java
@@ -0,0 +1,14 @@
+package com.example.twapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAcount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_acount);
+    }
+}
--- app/src/main/java/com/example/twapp/Logins.java
+++ app/src/main/java/com/example/twapp/Logins.java
@@ -2,14 +2,29 @@ package com.example.twapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Logins extends AppCompatActivity {
+    private Button btn_create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         System.out.println("oncreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logins);
+        btn_create=findViewById(R.id.createAccount);
+
+        btn_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Logins.this,CreateAcount.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
