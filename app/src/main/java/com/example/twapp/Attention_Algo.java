56598eb62e0df1a1e2d7b705377d92bc007c5d3f
Youxizhou <a641556378@gmail.com>
2022-12-16 18:08:53 +0800

    Strategy多了彈出警示訊息

--- app/src/main/java/com/example/twapp/Attention_Algo.java
+++ app/src/main/java/com/example/twapp/Attention_Algo.java
@@ -1,20 +1,49 @@
 package com.example.twapp;
 
+import static java.lang.Boolean.*;
+
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.widget.Button;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class Attention_Algo implements Algorithms {
-
-    @Override
-    public void setAlarm(TextView hr) {
-        hr.setTextColor(Color.rgb(210, 200, 0));
+    static boolean d = true;
+    Button negative ,neutral , possitive;
+    public void setAlarm(TextView hr, AlertDialog.Builder dialog) {
+        hr.setBackgroundColor(Color.rgb(210, 200, 0));
         hr.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+        if(d) {
+
+            dialog.setTitle("                 Notice!");
+            dialog.setIcon(android.R.drawable.ic_dialog_info);
+            dialog.setMessage("Seems that your heart rate is unusual\nAre you exercising?");
+            dialog.setPositiveButton("No", null);
+            dialog.setNeutralButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    d = false;
+                }
+            });
+            //dialog.setNegativeButton("No" , null);
+            AlertDialog ad = dialog.create();
+            ad.show();
+            neutral = ad.getButton(AlertDialog.BUTTON_NEUTRAL);
+            neutral.setBackgroundColor(Color.rgb(0,0,0));
+            possitive = ad.getButton(AlertDialog.BUTTON_POSITIVE);
+            possitive.setBackgroundColor(Color.rgb(0,0,0));
+
+        }
     }
 }
