5d116fe758505aad02a178acbdd67de809ed54f3
Yang <s0931861498@gmail.com>
2022-12-16 20:01:04 +0800

    complete-again

--- app/src/main/java/com/example/twapp/Command/Command.java
+++ app/src/main/java/com/example/twapp/Command/Command.java
@@ -1,7 +1,7 @@
 package com.example.twapp.Command;
 
 public abstract class Command {
-    Receiver receiver;
+    protected Receiver receiver;
     public Command(Receiver receiver){
         this.receiver = receiver;
     }
--- app/src/main/java/com/example/twapp/Command/Receiver.java
+++ app/src/main/java/com/example/twapp/Command/Receiver.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 
 public class Receiver {
-    Context context;
+    protected Context context;
     public Receiver(Context context){
         this.context = context;
     }
